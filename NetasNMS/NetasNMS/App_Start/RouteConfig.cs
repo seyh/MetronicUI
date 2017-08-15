d8241f1ab2183f83ace3b644ec70412285e59225
Veysel GOKCEN <vgokcen@NETAS.com.tr>
2017-08-15 13:20:11 +0300

    Create Login screen and routing

--- NetasNMS/NetasNMS/App_Start/RouteConfig.cs
+++ NetasNMS/NetasNMS/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NetasNMS
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Dashboard", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Login", action = "Login", id = UrlParameter.Optional }
             );
         }
     }
