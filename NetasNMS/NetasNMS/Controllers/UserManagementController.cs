1ce363b4502daca44a5176143cb18cdf6fe60167
Veysel GOKCEN <vgokcen@NETAS.com.tr>
2017-11-13 20:05:50 +0300

    Create User Management Pages

---
+++ NetasNMS/NetasNMS/Controllers/UserManagementController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NetasNMS.Controllers
+{
+    public class UserManagementController : Controller
+    {
+        // GET: UserManagement
+        public ActionResult UserManagement()
+        {
+            return View();
+        }
+
+        public ActionResult UserAudit()
+        {
+            return View();
+        }
+
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        public ActionResult SessionSetting()
+        {
+            return View();
+        }
+
+        public ActionResult AccountSetting()
+        {
+            return View();
+        }
+    }
+}
